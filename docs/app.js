6c06e4cc34e46eb664c2e8abcb85d1b8e3a8f645
Arnthorny <chidubem.anthony26@gmail.com>
2022-05-24 17:42:01 +0100

    Comments Added and Minor Bug Fix

--- docs/app.js
+++ docs/app.js
@@ -1,7 +1,6 @@
 "use strict";
 
-// import questions from "./questions.js";
-
+//This a test array for the questions and answers
 const questionsForGame = [
   {
     question:
@@ -25,10 +24,12 @@ const questionsForGame = [
   },
 ];
 
+//This array contains the test cash prizes to be won
 const cashToWin = [
   0, 5000, 7500, 10000, 15000, 20000, 30000, 45000, 70000, 120000,
 ];
 
+//The DOM elements to be used are selected here
 const startButton = document.querySelector(".gameStart");
 const dropCloseBtn = document.querySelector(".dropbtn");
 const dropIcons = document.querySelectorAll(".drop");
@@ -41,13 +42,14 @@ const allOptionButtonEl = document.querySelectorAll(".option");
 const questionNumber = document.querySelector(".questionNo");
 const cashWonEl = document.querySelector(".money");
 
+//Global variables are declared
 let qIndex;
 let qnA;
 let cashWon;
-
 let currentSelection;
 let randomIndex;
 
+//This function adds or removes event listeners
 function listenerToggle(domEl, removeOrAdd, funcName) {
   if (removeOrAdd == "add") {
     domEl.addEventListener("click", funcName);
@@ -56,63 +58,70 @@ function listenerToggle(domEl, removeOrAdd, funcName) {
   }
 }
 
+//This function toggles the display of the lifeline buttons and container
 const lifelineToggle = function () {
   dropIcons[0].classList.toggle("hidden");
   dropIcons[1].classList.toggle("hidden");
   lifeLinesBox.classList.toggle("hidden");
   overlay.classList.toggle("hidden");
-  console.log("toggleChek");
 };
 
+//This function executes when a lifeline button is clicked on
 function lifeLineUsed() {
   this.classList.add("used");
-  let y = currentSelection.options[1];
-  let everyOther = [...allOptionTextEl].filter((x) => x.textContent !== y);
+  const y = currentSelection.options[1];
+  const everyOther = [...allOptionTextEl].filter((x) => x.textContent !== y); //Selects all other options
   for (let i = 0; i < 2; i++) {
     let j = Math.floor(Math.random() * everyOther.length);
     everyOther[j].textContent = "";
     everyOther[j].parentElement.removeEventListener("click", checker);
     everyOther.splice(j, 1);
-  }
+  } //Loop removes any two random options
   lifelineToggle();
   listenerToggle(dropCloseBtn, "remove", lifelineToggle);
-  this.removeEventListener("click", lifeLineUsed);
+  this.removeEventListener("click", lifeLineUsed); //remove listener from used lifeline
 }
 
-//   // lifeLinesBox.style.display = dropIcons[0].classList.contains("hidden")
-//   //   ? "block"
-//   //   : "none";
-// });
-
+//This function checks the hasWon state (whether a player lost, won or walked away)
 const hasWon = function (win = true, walkaway = false) {
   listenerToggle(dropCloseBtn, "remove", lifelineToggle);
   for (const btns of allOptionButtonEl) {
     listenerToggle(btns, "remove", checker);
   }
   cashWonEl.parentElement.classList.add("hidden");
+  //The above code will executes regardless of the hasWon state
+
+  //These conditionals execute based on the hasWon state
   if (win) {
     questionNumber.textContent = `YOU HAVE WON \u20A6${cashWon.toLocaleString()}`;
   } else if (walkaway) {
     questionNumber.textContent = `YOU WALKED AWAY WITH \u20A6${cashWon.toLocaleString()}`;
   } else {
-    let amountWon = cashWon >= 250000 ? 250000 : cashWon >= 10000 ? 10000 : 0;
-    console.log(amountWon);
+    const y = [...allOptionTextEl].find(
+      (x) => x.textContent === currentSelection.options[1]
+    );
+    y.parentElement.style.backgroundColor = "green";
+    const amountWon = cashWon >= 250000 ? 250000 : cashWon >= 10000 ? 10000 : 0;
     questionNumber.textContent = `YOU HAVE WON \u20A6${amountWon.toLocaleString()}`;
   }
+  //Change the event listener function from 'walk' to 'begin' so as to restart game
+  //This will execute regardless of hasWon state
   startButton.removeEventListener("click", walk);
   startButton.childNodes[0].textContent = "RESTART GAME";
   startButton.addEventListener("click", begin);
 };
 
+//This function is in charge of most of display on game page
+//It takes three parameters: current qnA, if the game is still on, if user has walked
 function renderQ(selection, gameplay = true, walkaway = false) {
   if (selection && selection.question && gameplay && !walkaway) {
-    cashWon = cashToWin[qIndex];
+    cashWon = cashToWin[qIndex]; //Displays the amount that has been won
     cashWonEl.textContent = cashWon.toLocaleString();
     qIndex++;
-    questionNumber.textContent = qIndex;
+    questionNumber.textContent = qIndex; //update current question number
     questionNumber.classList.remove("hidden");
-    questionEL.textContent = selection.question;
-    listenerToggle(dropCloseBtn, "add", lifelineToggle);
+    questionEL.textContent = selection.question; //update current question
+    listenerToggle(dropCloseBtn, "add", lifelineToggle); //add an event handler to the lifeline dropdown
 
     const options = [...selection.options];
     for (const x of allOptionTextEl) {
@@ -121,20 +130,18 @@ function renderQ(selection, gameplay = true, walkaway = false) {
       x.parentElement.style.backgroundColor = "";
       options.splice(randomIndex, 1);
       x.parentElement.addEventListener("click", checker);
-    }
-  } else {
+    } //This loop randomly distributes options and adds an event listener to their buttons
   }
   if (walkaway) {
-    hasWon(false, true);
+    hasWon(false, true); //Call hasWon function if player walke away
   } else if (!(selection && selection.question)) {
-    hasWon();
+    hasWon(); //if the questions are exhausted, player has won!!
   } else {
-    console.log("Smooth");
+    console.log("Smooth"); //For debugging
   }
 }
 
-// optionList.classList();
-
+//This function generates a random index for the next question
 const nextQuestion = function () {
   randomIndex = Math.trunc(Math.random() * qnA.length);
   currentSelection = { ...qnA.splice(randomIndex, 1)[0] };
@@ -142,43 +149,45 @@ const nextQuestion = function () {
   return currentSelection;
 };
 
+//This function checks the player's answer
 function checker() {
   const ansPick = this.children.item(1).textContent;
-  const randomFlash = Math.trunc(Math.random() * 4000);
+  const randomFlash = Math.trunc(Math.random() * 4000); //count for yellow flash
+  this.classList.add("flash"); //This adds the flashing background
 
-  this.classList.add("flash");
+  //Execute this function after randomFlash count
   setTimeout(() => {
-    console.log(randomFlash);
     this.classList.remove("flash");
-    let correctOrNot = ansPick === currentSelection.options[1];
-    this.style.backgroundColor = correctOrNot ? "green" : "red";
+    const correctOrNot = ansPick === currentSelection.options[1]; //check option picked
+    this.style.backgroundColor = correctOrNot ? "green" : "red"; //set color accordingly
 
     if (correctOrNot) {
       setTimeout(() => {
         renderQ(nextQuestion(), correctOrNot);
-      }, 1000);
-    } else hasWon(false);
+      }, 1000); //Move to next question after 1 second
+    } else hasWon(false); //else call hasWon function after flashing
   }, randomFlash);
 }
 
-// const optionCopy = [...options];
-
+//Call this function if the player walks away
 function walk() {
-  renderQ(null, false, true);
+  renderQ(null, false, true); //render based on parameters
   for (const op of allOptionTextEl) {
     if (op.textContent === currentSelection.options[1]) {
       op.parentElement.style.backgroundColor = "green";
       break;
-    }
+    } //show the user the correct option
   }
 }
 
+//This function starts or restarts the game
 function begin() {
-  qnA = [...questionsForGame];
-  questionNumber.textContent = qIndex = 0;
+  qnA = [...questionsForGame]; //destructure array of questions of object and store here
+  questionNumber.textContent = qIndex = 0; //set current question number to 0
 
-  renderQ(nextQuestion());
+  renderQ(nextQuestion()); //display question and options
 
+  // add event listeners to all options, lifelines and lifelines dropdown.
   for (const btns of allOptionButtonEl) {
     listenerToggle(btns, "add", checker);
   }
@@ -188,11 +197,14 @@ function begin() {
   }
   dropCloseBtn.addEventListener("click", lifelineToggle);
 
+  cashWonEl.parentElement.classList.remove("hidden");
+  //Change event listener of start button to 'walk' function
   startButton.removeEventListener("click", begin);
   startButton.childNodes[0].textContent = "WALK AWAY";
 
   startButton.addEventListener("click", walk);
-  console.log("done");
+  console.log("done"); //for debugging
 }
 
+//This sets the ball rolling
 startButton.addEventListener("click", begin);
